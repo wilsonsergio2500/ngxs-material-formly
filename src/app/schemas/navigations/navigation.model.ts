ed41502580509848ceb4080c4249d82dfb8dd46b
steph <steph@DESKTOP-I5LT802>
2021-02-21 21:33:47 -0600

    tune up

--- src/app/schemas/navigations/navigation.model.ts
+++ src/app/schemas/navigations/navigation.model.ts
@@ -2,10 +2,6 @@ import { IFireBaseEntity } from '../../firebase/types/firebase-entity';
 
 export interface INavigationFirebaseModel extends IFireBaseEntity {
     navigationRoot: INavigationModel[];
-    //label: string;
-    //url: string;
-    //children: INavigationFirebaseModel[]
-    //IsLabelOnly: boolean;
 }
 
 export interface INavigationModel {
